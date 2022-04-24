086da229c377481c24af0fa38c83e3f5fc15134d
k-fujimura-ring <69615045+nyappy15ch@users.noreply.github.com>
2022-04-25 00:06:41 +0900

    wip:画像の切り取りコンポーネント作成

--- src/pages/crop/index.vue
+++ src/pages/crop/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    {{ text }}
+    <input type="range" id="volume" min='10' max='400' v-model='scale' @input='onInputScale'>
+    <input type='file' id="file" @input='onInputFile'>
+    <canvas ref='cvs' id='cvs' width='300' height='400' />
+    <button @click='onCropImg' v-text='"切り取り"' />
+    <canvas ref='out' id='out' width='200' height='200' />
   </div>
 </template>
 
@@ -11,6 +15,36 @@ export default Vue.extend({
   name: 'crop',
   data: () => ({
     text: 'crop page is running',
-  })
+    scale: 0,
+  }),
+  methods: {
+    onCropImg(): void {
+      console.log('crop image')
+    },
+    onInputScale():void {
+      console.log('change')
+    },
+    onInputFile(event: { target: HTMLInputElement }): void {
+      const cvs = this.$refs.cvs
+      const cw = cvs.width
+      const ch = cvs.height
+      const out = this.$refs.out
+      const oh = out.height
+      const ow = out.width
+      const img  = new Image()
+      img.onload = () => {   // 画像が読み込まれた
+        ix = img.width  / 2
+        iy = img.height / 2
+        let scl = Math.floor(cw / img.width * 100)
+        this.scale = scl
+      }
+      // const img = event.target.files[0]
+
+      let ix = 0    // 中心座標
+      let iy = 0
+      let v = 1.0   // 拡大縮小率
+      console.log('event', img.width)
+    }
+  }
 })
 </script>
