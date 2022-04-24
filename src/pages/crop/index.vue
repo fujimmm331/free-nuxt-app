cfe54db59120f28f84ef8e16d405ffbe817dfe8a
k-fujimura-ring <69615045+nyappy15ch@users.noreply.github.com>
2022-04-24 21:02:45 +0900

    feat:Innitial commit

---
+++ src/pages/crop/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ text }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'crop',
+  data: () => ({
+    text: 'crop page is running',
+  })
+})
+</script>
