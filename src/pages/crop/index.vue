9c94c63ab30aa2c093ef9676c6359d9bb02117d0
k-fujimura-ring <69615045+nyappy15ch@users.noreply.github.com>
2022-05-05 00:18:37 +0900

    feat:画像の倍率を可変にできるようにした

--- src/pages/crop/index.vue
+++ src/pages/crop/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <input type="range" id="volume" min='10' max='400' v-model='scale' @input='onInputScale'>
+    <input type="range" id="volume" min='100' max='400' v-model='scale' @input='onInputScale'>
     <input type='file' id="file" @input='onInputFile'>
-    <canvas ref='cvs' id='cvs' width='300' height='400' />
-    <button @click='onCropImg' v-text='"切り取り"' />
+    <canvas class='canvas' ref='cvs' id='cvs' width='300' height='400' />
+    <button @click='exportImage' v-text='"切り取り"' />
     <canvas ref='out' id='out' width='200' height='200' />
   </div>
 </template>
@@ -16,35 +16,56 @@ export default Vue.extend({
   data: () => ({
     text: 'crop page is running',
     scale: 0,
+    image: null,
   }),
   methods: {
     onCropImg(): void {
       console.log('crop image')
     },
     onInputScale():void {
-      console.log('change')
+      console.log('change', this.scale / 100)
+      this.drawImage(this.image, this.scale / 100)
     },
     onInputFile(event: { target: HTMLInputElement }): void {
-      const cvs = this.$refs.cvs
-      const cw = cvs.width
-      const ch = cvs.height
-      const out = this.$refs.out
-      const oh = out.height
-      const ow = out.width
-      const img  = new Image()
-      img.onload = () => {   // 画像が読み込まれた
-        ix = img.width  / 2
-        iy = img.height / 2
-        let scl = Math.floor(cw / img.width * 100)
-        this.scale = scl
-      }
-      // const img = event.target.files[0]
+      const file = event.target.files[0]
+      const image = new Image()
+      const reader = new FileReader()
 
-      let ix = 0    // 中心座標
-      let iy = 0
-      let v = 1.0   // 拡大縮小率
-      console.log('event', img.width)
+      this.scale = 100
+      reader.readAsDataURL(file)
+      reader.onload = (e) => {
+        image.src = e.target.result as string
+        image.onload = () => {
+          this.drawImage(image)
+        }
+      }
+
+      this.image = image
+    },
+    drawImage(image: HTMLImageElement, scale = 1) {
+      const canvas = this.$refs.cvs
+      canvas.width = image.width
+      canvas.height = image.height
+      const ctx = canvas.getContext('2d')
+      ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width * scale, image.height * scale)
+      ctx.strokeStyle = 'red'
+      ctx.lineWidth = 10
+      ctx.strokeRect( 0, 1, image.width, image.height / 1.2 ) // 赤い枠
+    },
+    exportImage() {
+      const canvas = this.$refs.cvs
+      const link = document.createElement('a');
+
+      link.download = 'croppedImage.png';
+      link.href = canvas.toDataURL()
+      link.click();
+      console.log('export')
     }
   }
 })
 </script>
+<style lang="css">
+.canvas {
+  width: 400px;
+}
+</style>
