b99f9ad0ac9e78b43f59f831564207f3678e96b3
k-fujimura-ring <69615045+nyappy15ch@users.noreply.github.com>
2022-05-05 17:39:47 +0900

    feat: pretterを効くようにした

--- src/pages/crop/index.vue
+++ src/pages/crop/index.vue
@@ -1,39 +1,66 @@
 <template>
   <div>
-    <input type="range" id="volume" min='100' max='400' v-model='scale' @input='onInputScale'>
-    <input type='file' id="file" @input='onInputFile'>
-    <canvas class='canvas' ref='cvs' id='cvs' width='300' height='400' />
-    <button @click='exportImage' v-text='"切り取り"' />
-    <canvas ref='out' id='out' width='200' height='200' />
+    <input
+      type="range"
+      id="volume"
+      min="100"
+      max="400"
+      v-model="scale"
+      @input="onInputScale"
+    />
+    <input type="file" id="file" @input="onInputFile" />
+    <canvas class="canvas" ref="cvs" id="cvs" width="300" height="400" />
+    <button @click="exportImage" v-text="'切り取り'" />
+    <canvas ref="out" id="out" width="200" height="200" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+interface ComponentData {
+  scale: number
+  image: HTMLImageElement | null
+}
+
 export default Vue.extend({
   name: 'crop',
-  data: () => ({
-    text: 'crop page is running',
-    scale: 0,
-    image: null,
-  }),
+  data: () =>
+    ({
+      text: 'crop page is running',
+      scale: 0,
+      image: null,
+    } as ComponentData),
   methods: {
+    initializeScale(): void {
+      this.scale = 100
+    },
     onCropImg(): void {
       console.log('crop image')
     },
-    onInputScale():void {
-      console.log('change', this.scale / 100)
+    onInputScale(): void {
+      if (!this.image) {
+        return
+      }
+
       this.drawImage(this.image, this.scale / 100)
     },
     onInputFile(event: { target: HTMLInputElement }): void {
+      if (!event.target.files) {
+        return
+      }
+
       const file = event.target.files[0]
       const image = new Image()
       const reader = new FileReader()
+      this.initializeScale()
 
-      this.scale = 100
       reader.readAsDataURL(file)
       reader.onload = (e) => {
+        if (!e.target) {
+          return
+        }
+
         image.src = e.target.result as string
         image.onload = () => {
           this.drawImage(image)
@@ -43,25 +70,48 @@ export default Vue.extend({
       this.image = image
     },
     drawImage(image: HTMLImageElement, scale = 1) {
-      const canvas = this.$refs.cvs
+      const canvas = this.$refs.cvs as HTMLCanvasElement
+      if (!canvas) {
+        return
+      }
+
       canvas.width = image.width
       canvas.height = image.height
       const ctx = canvas.getContext('2d')
-      ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width * scale, image.height * scale)
+
+      if (!ctx) {
+        return
+      }
+
+      ctx.drawImage(
+        image,
+        0,
+        0,
+        image.width,
+        image.height,
+        0,
+        0,
+        image.width * scale,
+        image.height * scale,
+      )
       ctx.strokeStyle = 'red'
       ctx.lineWidth = 10
-      ctx.strokeRect( 0, 1, image.width, image.height / 1.2 ) // 赤い枠
+      ctx.strokeRect(0, 1, image.width, image.height / 1.2) // 赤い枠
     },
     exportImage() {
-      const canvas = this.$refs.cvs
-      const link = document.createElement('a');
+      const canvas = this.$refs.cvs as HTMLCanvasElement
+      const link = document.createElement('a')
 
-      link.download = 'croppedImage.png';
+      if (!canvas) {
+        return
+      }
+
+      link.download = 'croppedImage.png'
       link.href = canvas.toDataURL()
-      link.click();
+      link.click()
       console.log('export')
-    }
-  }
+    },
+  },
 })
 </script>
 <style lang="css">
