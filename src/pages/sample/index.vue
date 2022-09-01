575c6014b64e3619127b6ebe7cb9ed5d6d71e9cc
k-fujimura-ring <69615045+nyappy15ch@users.noreply.github.com>
2022-09-01 22:38:45 +0900

    feat:ボタンを押すと文字が大きくなりながらカウントが上がる仕組みを作成

--- src/pages/sample/index.vue
+++ src/pages/sample/index.vue
@@ -1,15 +1,48 @@
 <template>
-  <div>{{fuga}}</div>
+  <div class="bg-gray-50 h-auto timer-game p-2">
+    <div v-show="states.count > 0" :style="style">{{ states.count }}</div>
+    <button
+      class="transition bg-blue-400 text-white pt-5 pb-5 pl-10 pr-10 rounded-md text-lg ease-in-out hover:bg-indigo-500 duration-300 "
+      @click="onClickButton"
+      v-text='buttonText'
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
+import { computed, onMounted, reactive, ref } from 'vue'
 
-const hoge = reactive({
-  text: 'ほげ',
+type States = {
+  count: number
+  isCountUpping: boolean
+}
+
+const states = reactive<States>({
+  count: 0,
+  isCountUpping: false,
 })
 
-const fuga = ref(0)
+const onClickButton = () => {
+  states.isCountUpping = !states.isCountUpping
+  const timer = setInterval(() => {
+    states.count ++
+    if(!states.isCountUpping) clearInterval(timer)
+  }, 100)
+}
 
+const buttonText = computed(() => {
+  return states.isCountUpping ? 'すとっぷ' : 'すたと'
+})
+
+const style = computed(() => {
+  return `font-size: ${states.count}px;`
+})
 
 </script>
+<style>
+  .timer-game {
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+  }
+</style>
