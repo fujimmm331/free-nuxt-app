83989e50f8407ed8ee8984a1ecd356ea541dfba6
k-fujimura-ring <69615045+nyappy15ch@users.noreply.github.com>
2022-09-04 19:48:03 +0900

    feat:formのコンポーネントを作成

---
+++ src/components/template/user/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import UserForm from '@/components/molecules/form/user-form.vue';
+</script>
+
+<template>
+  <UserForm class='user' />
+</template>
+
+<style>
+  .user {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
