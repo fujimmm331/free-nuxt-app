d2844164a65b364bc87e790be9295f397a563336
fujimmm331 <nyappy15th@gmail.com>
2022-09-24 21:20:06 +0900

    feat:CHINCHIN, OCHINCHINを判定するロジックを追加した

--- src/store/HandStore/actions/useHandJudgeHand.ts
+++ src/store/HandStore/actions/useHandJudgeHand.ts
@@ -5,28 +5,53 @@ export default () => {
   const diceResult = useDiceIndexPageGetResult()
   const setHandState = useHandSetHand()
   const judgeHands = () => {
-    if (diceResult.value.includes('U') && diceResult.value.includes('N') && diceResult.value.includes('KO')) {
+    if (diceResult.value.includes('N') && diceResult.value.includes('KO')) {
+      judgeNkoHands()
+    }
+
+
+    if (diceResult.value.includes('N') && diceResult.value.includes('CHI')) {
+      judgeNchiHands()
+    }
+  }
+
+  const judgeNkoHands = () => {
+    if (diceResult.value.includes('U')) {
       setHandState('UNKO')
     }
 
-    if (diceResult.value.includes('MA') && diceResult.value.includes('N') && diceResult.value.includes('KO')) {
+    if (diceResult.value.includes('MA')) {
       setHandState('MANKO')
     }
 
-    if (diceResult.value.includes('CHI') && diceResult.value.includes('N') && diceResult.value.includes('KO')) {
+    if (diceResult.value.includes('CHI')) {
       setHandState('CHINKO')
     }
 
-    if (diceResult.value.includes('O') && diceResult.value.includes('CHI') && diceResult.value.includes('N') && diceResult.value.includes('KO')) {
+    if (diceResult.value.includes('O') && diceResult.value.includes('CHI')) {
       setHandState('OCHINKO')
     }
 
-    if (diceResult.value.includes('O') && diceResult.value.includes('MA') && diceResult.value.includes('N') && diceResult.value.includes('KO')) {
+    if (diceResult.value.includes('O') && diceResult.value.includes('MA')) {
       setHandState('OMANKO')
     }
 
-    if (diceResult.value.includes('U') && diceResult.value.includes('N') && diceResult.value.includes('CHI')) {
+  }
+
+  const judgeNchiHands = () => {
+    if (diceResult.value.includes('U')) {
       setHandState('UNCHI')
+    }
+
+    const numOfCHI = diceResult.value.filter((value) => value === 'CHI').length
+    const numOfN = diceResult.value.filter((value) => value === 'N').length
+
+    if (numOfCHI >= 2 && numOfN >= 2 && diceResult.value.includes('O')) {
+      setHandState('OCHINCHIN')
+    }
+
+    if (numOfCHI >= 2 && numOfN >= 2) {
+      setHandState('CHINCHIN')
     }
   }
 
