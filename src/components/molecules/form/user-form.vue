0b565a9427905b8d869fde79534a8cad606eef65
fujimmm331 <nyappy15th@gmail.com>
2022-10-23 16:56:54 +0900

    feat:v-formによるフォームの作成

--- src/components/molecules/form/user-form.vue
+++ src/components/molecules/form/user-form.vue
@@ -1,60 +1,45 @@
 <script setup lang="ts">
-  import { reactive, ref } from 'vue';
-  import { email, required } from '@/utils/validations/index'
+import { reactive, ref } from 'vue';
+import { email, required } from '@/utils/validations/index'
+import { Nullable } from '@/types';
 
-  const state = reactive({
-    form: {
-      email: null,
-      name: null,
-      sex: null,
-    }
-  })
+type VFormType = {
+  validate: () => void
+  reset: () => void
+  resetValidation: () => void
+}
 
-  const forms = ref(null)
-
-  const onSubmit = () => {
-    const validateResult = (forms.value as any).validate()
-    if (!validateResult) return
-
-    console.log('submit!', validateResult)
+const state = reactive({
+  form: {
+    email: null,
+    name: null,
+    sex: null,
   }
-  </script>
-  
-  <template>
-    <div>
-      <v-form ref="forms" @submit.prevent="onSubmit">
-        <v-text-field
-          v-model="state.form.name"
-          label="ユーザー名"
-          :rules=[required]
-          required
-        />
-        <v-text-field
-          v-model="state.form.email"
-          :rules="[email, required]"
-          label="メールアドレス"
-        />
-        <v-radio-group v-model="state.form.sex">
-          <v-radio
-            label="男"
-            :value=1
-          />
-          <v-radio
-            label="女"
-            :value=2
-          />
-          <v-radio
-            label="その他"
-            :value=3
-          />
-        </v-radio-group>
+})
 
-        <v-btn
-          v-text='"送信"'
-          color="primary"
-          elevation="2"
-          @click="onSubmit"
-        />
-      </v-form>
-    </div>
-  </template>
+const forms = ref<Nullable<VFormType>>(null)
+
+const onSubmit = () => {
+  console.log('forms', forms.value)
+  const validateResult = forms.value?.validate()
+  if (!validateResult) return
+
+  console.log('submit!', validateResult)
+}
+</script>
+  
+<template>
+  <div>
+    <v-form ref="forms" @submit.prevent="onSubmit">
+      <v-text-field v-model="state.form.name" label="ユーザー名" :rules=[required] required />
+      <v-text-field v-model="state.form.email" :rules="[email, required]" label="メールアドレス" />
+      <v-radio-group v-model="state.form.sex">
+        <v-radio label="男" :value=1 />
+        <v-radio label="女" :value=2 />
+        <v-radio label="その他" :value=3 />
+      </v-radio-group>
+
+      <v-btn v-text='"送信"' color="primary" elevation="2" @click="onSubmit" />
+    </v-form>
+  </div>
+</template>
