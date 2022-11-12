a16033806745cdd1309a9f59f1f87af6fd438e5e
fujimmm331 <nyappy15th@gmail.com>
2022-11-12 15:00:44 +0900

    feat:ベースとなるテキストエリアコンポーネントを作成

---
+++ src/components/atoms/FreeTextarea/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-textarea v-model="inputValue" />
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from '@nuxtjs/composition-api'
+
+type PropsType = {
+  value: string
+}
+
+export default defineComponent({
+  props: {
+    value: { type: String, required: true },
+  },
+  setup(props: PropsType, context) {
+    const inputValue = computed({
+      get: () => props.value,
+      set: (value) => context.emit('input', value),
+    })
+
+    return {
+      inputValue,
+    }
+  },
+})
+</script>
