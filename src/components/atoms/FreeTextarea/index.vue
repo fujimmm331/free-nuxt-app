64adfd0c3e908261b776a512ef6d1b696a2f27af
fujimmm331 <nyappy15th@gmail.com>
2022-11-13 18:18:09 +0900

    feat:propsの設定

--- src/components/atoms/FreeTextarea/index.vue
+++ src/components/atoms/FreeTextarea/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <v-textarea v-model="inputValue" @keydown="onKeydown" />
+  <v-textarea
+    v-model="inputValue"
+    :disabled="$props.disabled"
+    :append-icon="$props.appendIcon"
+    :auto-grow="$props.autoGrow"
+    :clearable="$props.clearable"
+    :readonly="$props.readonly"
+    @keydown="onKeydown"
+  />
 </template>
 
 <script lang="ts">
@@ -8,16 +16,22 @@ import { handleCtrlEnter } from '@/utils/emits/CtrlEnter'
 
 type PropsType = {
   value: string
+  disabled: boolean
 }
 
 export default defineComponent({
   props: {
     value: { type: String, required: true },
+    disabled: { type: Boolean, required: false, default: false },
+    appendIcon: { type: String, required: false },
+    autoGrow: { type: Boolean, required: false, default: false },
+    clearable: { type: Boolean, required: false, default: true },
+    readonly: { type: Boolean, required: false, default: false },
   },
   setup(props: PropsType, context) {
     const inputValue = computed({
       get: () => props.value,
-      set: (value) => context.emit('input', value),
+      set: (value) => context.emit('input', value || ''), // clearableを押すとnullが入るので空文字にキャストする
     })
 
     const onKeydown = (e: KeyboardEvent) => handleCtrlEnter(e, context)
