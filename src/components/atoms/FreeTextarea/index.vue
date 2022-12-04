1dd6d8bfe312d5f3668bdd99eddda915f7bb6d5e
fujimmm331 <nyappy15th@gmail.com>
2022-12-04 19:07:18 +0900

    fix:挿入後focusoutしていまうため、挿入したテキスト分いれてfocusするようにした

--- src/components/atoms/FreeTextarea/index.vue
+++ src/components/atoms/FreeTextarea/index.vue
@@ -2,7 +2,7 @@
   <div class="free-textarea">
     <v-textarea
       v-model="inputValue"
-      ref="textarea"
+      ref="vTextarea"
       :disabled="$props.disabled"
       :append-icon="$props.appendIcon"
       :auto-grow="$props.autoGrow"
@@ -14,10 +14,17 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from '@nuxtjs/composition-api'
+import {
+  computed,
+  defineComponent,
+  nextTick,
+  onMounted,
+  ref,
+} from '@nuxtjs/composition-api'
 import { handleCtrlEnter } from '@/utils/emits/CtrlEnter'
 import { VTextarea } from 'vuetify/lib'
 import { useSelections } from '@/composable/atoms/FreeTextarea/useSelections'
+import { Nullable } from '@/types/utils/nullable'
 
 type PropsType = {
   value: string
@@ -34,42 +41,80 @@ export default defineComponent({
     readonly: { type: Boolean, required: false, default: false },
   },
   setup(props: PropsType, context) {
-    const { selectionStart, selectionEnd, updateStart, updateEnd } =
-      useSelections()
-    const textarea = ref<InstanceType<typeof VTextarea>>()
+    const {
+      selectionsState,
+      updateStart,
+      updateEnd,
+      setElement,
+      setSelectionRange,
+      focus,
+    } = useSelections()
+    const vTextarea = ref<InstanceType<typeof VTextarea>>()
     const inputValue = computed({
       get: () => props.value,
       set: (value) => {
         context.emit('input', value || '')
-        const input = textarea.value?.$el.querySelector('textarea')
-        if (input) {
-          updateStart(input.selectionStart)
-          updateEnd(input.selectionEnd)
-        }
       }, // clearableを押すとnullが入るので空文字にキャストする
     })
 
+    // functions
+    /**
+     * selectionStart, endを更新する関数
+     */
+    const updateSelections = (selectionStart: number, selectionEnd: number) => {
+      updateStart(selectionStart)
+      updateEnd(selectionEnd)
+    }
+
+    /**
+     * コントロールエンターで発火させるための関数
+     */
     const onKeydown = (e: KeyboardEvent) => handleCtrlEnter(e, context)
 
-    // 外部からテキストを挿入するための関数
+    // expose functions
+    /**
+     * カーソル位置にテキストを挿入するための関数
+     */
     const insertTexts = (insertText: string) => {
+      if (!selectionsState.element) return
+      
+      const currentSelectionStart = selectionsState.element.selectionStart!
+      const currentSelectionEnd = selectionsState.element.selectionEnd!
+
+      // マウスなどでカーソルを変更したときに追えない可能性があるため、insert前に保存する
+      updateSelections(currentSelectionStart, currentSelectionEnd)
+
       const beforeValue = `${inputValue.value.substring(
         0,
-        selectionEnd.value!,
+        selectionsState.selectionEnd!,
       )}`
       const afterValue = `${inputValue.value.substring(
-        selectionEnd.value!,
+        selectionsState.selectionEnd!,
         inputValue.value.length,
       )}`
       const newValue = `${beforeValue}${insertText}${afterValue}`
       inputValue.value = newValue
+
+      focus()
+      nextTick(() => {
+        updateSelections(
+          currentSelectionStart + insertText.length,
+          currentSelectionEnd + insertText.length,
+        )
+        setSelectionRange()
+      })
     }
+
+    onMounted(() => {
+      const element = vTextarea.value?.$el.querySelector('textarea')
+      if (!element) return
+      setElement(element)
+    })
+
     return {
       inputValue,
       onKeydown,
-      textarea,
-      selectionStart,
-      selectionEnd,
+      vTextarea,
       insertTexts,
     }
   },
